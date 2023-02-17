b771c578af04409599fdd657985f9ebc6aee416b
oliversulyok <sulyokoliver@msn.com>
2023-02-17 15:34:39 +0100

    Started implementing TDD

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import {render, screen} from '@testing-library/react'
+import App from "./App"
+import React from 'react'
+
+describe('App', () => {
+test('Do nothing', async () => {
+    render(<App />);
+})
+})

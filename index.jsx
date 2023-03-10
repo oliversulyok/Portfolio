2b1b71ced7fad287c63b126fae834b7512b359db
oliversulyok <sulyokoliver@msn.com>
2023-03-10 09:39:41 +0100

    Corrected main folder

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import App from "./src/App";
-import React from "react";
+import React from 'react';
+import App from './src/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
